d757610c9d97be1ef09073e53febe2503dc4d752
Odede AbdulGhaffar <younggazzy187@gmail.com>
2023-07-12 16:01:09 +0100

    initial commit

---
+++ Altschool/assignment2/script.js
@@ -0,0 +1,28 @@
+
+// alert( "Im a Javascript")
+
+// //  alert[234=1] ;
+// // console.log( alert)
+
+// let admin, myName;
+//  myNameame = "John";
+//   admin = myName;
+// alert(admin)
+'use strict'
+// let message, myPlanet ;
+// myPlanet = "Earth";
+// message = myPlanet;
+// alert(message)
+
+// constant sample
+// function birthday {
+    // const birthday = "18.04.1993";
+// const age = someCode(birthday);
+// return age}k
+
+// if else statement
+let officialName = prompt( "what is the official name of JavaScript");
+ if (officialName == "ECMAScript")
+ { alert ("Right");}
+ else { alert("You don't know? ECMAScript!")};
+// alert('Right!') : alert( "You don't know? ECMAScript!" )
